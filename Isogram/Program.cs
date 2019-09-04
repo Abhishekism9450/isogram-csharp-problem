ac8fd3fc2853eb923854bb3e8b0462061c392a3b
a.chaudhary <a.chaudhary@cgi.com>
2019-09-04 12:31:18 +0530

    isogram-done

---
+++ Isogram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Isogram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public static bool isogram(string text)
+        {
+            HashSet<char> myset = new HashSet<char>();
+
+            for(int c = 0; c < text.Length; c++)
+            {
+                if((text[c]==' ') || (text[c]== '-'))
+                {
+                    continue;
+                }
+                if(myset.Contains(text[c])){
+                    return false;
+                }
+                else
+                {
+                    myset.Add(text[c]);
+                }
+            }
+            return true;
+        }
+    }
+}
